e674c0a9a51775bd8378b1c3571b1bd7ef4885f4
Drew Tucker <dtuck43@gmail.com>
2018-03-30 12:51:28 -0700

    Add edit entry form, not yet functioning.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { EntryListComponent } from './entry-list/entry-list.component';
+import { EditEntryComponent } from './edit-entry/edit-entry.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    EntryListComponent
+    EntryListComponent,
+    EditEntryComponent
   ],
   imports: [
     BrowserModule
